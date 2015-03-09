f54e39fbfc9a830d13e383b433d1b8b196b8ab2f
Na, Hyunik <wing754@naver.com>
2015-03-09 14:12:28 +0900

    [TASK] Renaming a directory name (commons --> common) [DESC]

--- apps/dashboard/src/js/config.js
+++ apps/dashboard/src/js/config.js
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2012-2015 S-Core Co., Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,10 +17,10 @@
 /* exported require, WS_INFO_PATH */
 /* jshint -W079 */
 
-var webidaHost = 
+var webidaHost =
     decodeURIComponent(document.cookie.replace(/(?:(?:^|.*;\s*)webida\.host\s*\=\s*([^;]*).*$)|^.*$/, '$1'));
 // var webida = '//library.' + webidaHost + '/webida/webida-0.3';
-var webida = '../../commons/src/webida/webida-0.3';
+var webida = '../../common/src/webida/webida-0.3';
 
 var require = {
     baseUrl: '.',
@@ -39,7 +39,7 @@ var require = {
         common: 'src/js/common',
         q: 'lib/q',
         async: 'lib/async',
-        'webida-lib': '../../commons/src/webida'
+        'webida-lib': '../../common/src/webida'
     }
 };
 
