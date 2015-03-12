4e483637f6418272dc5eb33e778298c09027eeb8
Na, Hyunik <wing754@naver.com>
2015-03-12 11:05:55 +0900

    [TASK] Applying changes applied to library but not to webida-client [DESC]

--- common/src/webida/plugins/workspace/plugin.js
+++ common/src/webida/plugins/workspace/plugin.js
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2012-2015 S-Core Co., Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -868,11 +868,11 @@ define([(time = timedLogger.getLoggerTime(), 'webida-lib/app'),
             }
         }
 
-        var tree = dom.byId('wv-tree');
-        tree.addEventListener('drop', onDragDrop, false);
-        tree.addEventListener('dragover', onDragOver, false);
-        tree.addEventListener('dragleave', onDragLeave, false);
-        tree.addEventListener('dragstart', onDragStart, false);
+        var wvTree = dom.byId('wv-tree');
+        wvTree.addEventListener('drop', onDragDrop, false);
+        wvTree.addEventListener('dragover', onDragOver, false);
+        wvTree.addEventListener('dragleave', onDragLeave, false);
+        wvTree.addEventListener('dragstart', onDragStart, false);
     }
 
     function removeInteractively() {
--- common/src/webida/widgets/views/viewmanager.js
+++ common/src/webida/widgets/views/viewmanager.js
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2012-2015 S-Core Co., Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -101,17 +101,22 @@ function (BorderContainer, ContentPane, topic) {
 
             var memberList, i;
 
-            var self = this;
+            var self = this, name = null;
             Object.keys(this._groupMap).forEach(function (key) {
+                if (name) {
+                    return;
+                }
+
                 memberList = self._groupMap[key];
                 for (i = 0 ; i < memberList.length; i++) {
                     if (memberList[i] === splitViewContainer) {
-                        return key;
+                        name = key;
+                        break;
                     }
                 }
             });
 
-            return null;
+            return name;
         },
 
         getGroupListByName : function (groupName) {
