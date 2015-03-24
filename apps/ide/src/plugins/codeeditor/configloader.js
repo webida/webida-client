47dd2123ba0c7c356ff5ce8424da2584e7514ebc
sewon0326.kim <sewon0326.kim@samsung.com>
2015-03-24 15:27:28 +0900

    [TASK] Remove irrelevant setting of an option [DESC.] .editorconfig does not have options for indentOnPaste.

--- apps/ide/src/plugins/codeeditor/configloader.js
+++ apps/ide/src/plugins/codeeditor/configloader.js
@@ -174,7 +174,6 @@ function (ide, _) {
             instance.setIndentWithTabs(config.indentWithTabs);
             instance.setIndentUnit(config.indentUnit);
             instance.setTabSize(config.tabSize);
-            instance.setIndentOnPaste(config.indentOnPaste);
             instance.setTrimTrailingWhitespaces(config.trimTrailingWhitespaces);
             instance.setInsertFinalNewLine(config.insertFinalNewLine);
         }
