6b482a37cf39851b29b77b254eaff7ec9b4fac04
hw.shim <hw.shim@samsung.com>
2015-05-20 21:24:38 +0900

    [BUGFIX] Path corrected [DESC.]

--- apps/ide/src/plugins/webida.editor.test-editor/unit-test.js
+++ apps/ide/src/plugins/webida.editor.test-editor/unit-test.js
@@ -1,5 +1,5 @@
 /* global QUnit */
-require(['./plugin'], function (plugin) {
+require(['plugins/webida.editor.test-editor/plugin'], function (plugin) {
     'use strict';
     QUnit.test('TestEditor API Test', function (assert) {
         assert.ok(typeof plugin.create === 'function', 'create method exists');
