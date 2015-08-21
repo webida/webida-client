64d88ad6f774cebfaa2efe91b4b24ecab83d8f11
Shim Heungwoon <hw.shim@samsung.com>
2015-08-21 20:06:06 +0900

    [TASK] PartContainerWidgetAdapter added

---
+++ common/src/webida/plugins/workbench/ui/PartContainerWidgetAdapter.js
@@ -0,0 +1,59 @@
+/*
+* Copyright (c) 2012-2015 S-Core Co., Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+ * Constructor
+ * PartContainerWidgetAdapter
+ *
+ * @see
+ * @since: 2015.07.15
+ * @author: hw.shim
+ */
+
+// @formatter:off
+define([
+    'external/eventEmitter/EventEmitter',
+    'webida-lib/util/genetic',
+    'webida-lib/util/logger/logger-client',
+    './WidgetAdapter'
+], function(
+    EventEmitter,
+    genetic, 
+    Logger,
+    WidgetAdapter
+) {
+    'use strict';
+// @formatter:on
+
+    /**
+     * @typedef {Object} DataSource
+     */
+
+    var logger = new Logger();
+    //logger.setConfig('level', Logger.LEVELS.log);
+    //logger.off();
+
+    function PartContainerWidgetAdapter() {
+        logger.info('new PartContainerWidgetAdapter()');
+    }
+
+
+    genetic.inherits(PartContainerWidgetAdapter, WidgetAdapter, {
+
+    });
+
+    return PartContainerWidgetAdapter;
+});
