0c49221be4a369c866eff0816dc32a3142369783
Shim Heungwoon <hw.shim@samsung.com>
2015-11-10 17:29:32 +0900

    [BUGFIX] DataSource is unregsistered when it is not used anymore by any EditorPart

--- common/src/webida/plugins/workbench/ui/partModelProvider.js
+++ common/src/webida/plugins/workbench/ui/partModelProvider.js
@@ -1,20 +1,20 @@
 /*
-* Copyright (c) 2012-2015 S-Core Co., Ltd.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2012-2015 S-Core Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-/**
+ /**
  * Constructor
  * partModelProvider
  *
@@ -30,7 +30,7 @@ define([
     'webida-lib/util/logger/logger-client',
     './PartModel',
     './PartRegistry'
-], function(
+], function (
     workbench,
     genetic, 
     Logger,
@@ -58,7 +58,7 @@ define([
          * @private
          * @return {Map.<DataSource, {Map.<Function, PartModel>}>}
          */
-        _getModelRegistry: function() {
+        _getModelRegistry: function () {
             return this.modelRegistry;
         },
 
@@ -66,7 +66,7 @@ define([
          * @private
          * @return {Map.<Function, PartModel>}
          */
-        _getModelsByDataSource: function(dataSource) {
+        _getModelsByDataSource: function (dataSource) {
             return this._getModelRegistry().get(dataSource);
         },
 
@@ -76,9 +76,9 @@ define([
          * @param {DataSource} dataSource
          * @param {PartModel} model
          */
-        register: function(dataSource, model) {
+        register: function (dataSource, model) {
             logger.info('register(' + dataSource + ', ' + model + ')');
-            if (!( model instanceof PartModel)) {
+            if (!(model instanceof PartModel)) {
                 throw new Error('model should implement PartModel interface');
             }
             var reg = this._getModelRegistry();
@@ -93,11 +93,11 @@ define([
          *
          * @param {PartModel} model
          */
-        unregister: function(model) {
+        unregister: function (model) {
             logger.info('unregister(' + model + ')');
             var reg = this._getModelRegistry();
-            reg.forEach(function(map) {
-                map.forEach(function(partModel, ModelClass) {
+            reg.forEach(function (map) {
+                map.forEach(function (partModel, ModelClass) {
                     if (model === partModel) {
                         map['delete'](ModelClass);
                     }
@@ -113,7 +113,7 @@ define([
          * @param {Function} PartModelClass
          * @return {PartModel}
          */
-        getPartModel: function(dataSource, PartModelClass) {
+        getPartModel: function (dataSource, PartModelClass) {
             logger.info('getPartModel(' + dataSource + ', ' + PartModelClass.name + ')');
             var modelsOfDs = this._getModelsByDataSource(dataSource);
             if (modelsOfDs) {
@@ -127,12 +127,12 @@ define([
          * @param {PartModel} model
          * @return {boolean}
          */
-        isModelUsed: function(model) {
+        isModelUsed: function (model) {
             var parts, result = false;
-            workbench.getPages().forEach(function(page) {
+            workbench.getPages().forEach(function (page) {
                 parts = page.getPartRegistry().getParts();
-                parts.forEach(function(partsByDs, ds) {
-                    partsByDs.forEach(function(part) {
+                parts.forEach(function (partsByDs/*, ds*/) {
+                    partsByDs.forEach(function (part) {
                         if (model === part.getModel()) {
                             result = true;
                         }
@@ -149,11 +149,11 @@ define([
          * @param {PartModel} model
          * @return {boolean}
          */
-        isDataSourceUsed: function(dataSource) {
+        isDataSourceUsed: function (dataSource) {
             var used = false;
             var provider = this;
             var modelsOfDs = this._getModelsByDataSource(dataSource);
-            modelsOfDs.forEach(function(model) {
+            modelsOfDs.forEach(function (model) {
                 if (provider.isModelUsed(model) === true) {
                     used = true;
                 }
@@ -167,14 +167,14 @@ define([
          * @param {PartModel} model
          * @return {boolean}
          */
-        isModelProvided: function(model) {
+        isModelProvided: function (model) {
             var provided = false, PartModelClass;
-            if (!( model instanceof PartModel)) {
+            if (!(model instanceof PartModel)) {
                 return false;
             }
             PartModelClass = model.constructor;
-            this._getModelRegistry().forEach(function(modelMap) {
-                modelMap.forEach(function(_model) {
+            this._getModelRegistry().forEach(function (modelMap) {
+                modelMap.forEach(function (_model) {
                     if (_model === model) {
                         provided = true;
                     }
@@ -188,28 +188,26 @@ define([
          *
          * @param {DataSource} dataSource
          */
-        unregisterModels: function(dataSource) {
+        unregisterModels: function (dataSource) {
             var provider = this;
             var modelsOfDs = this._getModelsByDataSource(dataSource);
-            modelsOfDs.forEach(function(model) {
+            modelsOfDs.forEach(function (model) {
                 provider.unregister(model);
             });
         },
 
-        _listenPartRegistry: function() {
+        _listenPartRegistry: function () {
             // @formatter:off
             var provider = this;
             var dsReg = workbench.getDataSourceRegistry();
             var page = workbench.getCurrentPage();
             var PartReg = page.getPartRegistry();
-            PartReg.on(PartRegistry.PART_UNREGISTERED, function(part) {
+            PartReg.on(PartRegistry.PART_UNREGISTERED, function (part) {
                 var dataSource = part.getDataSource();
                 var model = part.getModel();
-                if (provider.isModelProvided(model) === true
-                        && provider.isDataSourceUsed(dataSource) === false) {
-                    if (dataSource.isDeleted()) {
-                        dsReg.unregister(dataSource);
-                    }
+                if (provider.isModelProvided(model) === true &&
+                    provider.isDataSourceUsed(dataSource) === false) {
+                    dsReg.unregister(dataSource);
                     provider.unregisterModels(dataSource);
                 }
             });
