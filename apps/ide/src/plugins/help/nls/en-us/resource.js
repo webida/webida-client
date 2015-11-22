f6025d37142774f15c7ce4b1cf8610e1e0397140
김중훈 <jh1977.kim@samsung.com>
2015-11-22 14:11:51 +0900

    i18n - help plugin
     - added en-us message resources
     - refactoring with message creating codes

---
+++ apps/ide/src/plugins/help/nls/en-us/resource.js
@@ -0,0 +1,6 @@
+define({
+    aboutDialogTitle: 'About Webida IDE',
+    messageVersion: 'Version ({number})',
+    messageBuiltAt : 'Built at {builtAt}',
+    messageBuildCommitId: 'Commit ID {commitId}'
+});
---
+++ apps/ide/src/plugins/help/nls/resource.js
@@ -0,0 +1,9 @@
+define({
+    root: {
+        aboutDialogTitle: 'About Webida IDE',
+        messageVersion: 'Version ({number})',
+        messageBuiltAt : 'Built at {builtAt}',
+        messageBuildCommitId: 'Commit ID {commitId}'
+    },
+    'en-us': true
+});
