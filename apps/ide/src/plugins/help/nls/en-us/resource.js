e1915dee80f49b0f0749bf6401c01853a295c2ef
김중훈 <jh1977.kim@samsung.com>
2015-11-22 17:23:46 +0900

    [FEATURE] i18n - help plugin
     - part of resolving #723

---
+++ apps/ide/src/plugins/help/nls/en-us/resource.js
@@ -0,0 +1,10 @@
+define({
+    aboutDialogTitle: 'About Webida IDE',
+    messageVersion: 'Version ${version} (${buildNumber})',
+    messageBuiltAt : 'Built at ${buildTime}',
+    messageBuildCommitId: 'Commit ID ${commitId}',
+    '[menu] &About': '&About',
+    '[menu] Help Contents' : 'Help Contents',
+    '[menu] IDE Reference Manual' : 'IDE Reference Manual',
+    '[menu] API Documentation' : 'API Documentation'
+});
--- apps/ide/src/plugins/help/plugin.js
+++ apps/ide/src/plugins/help/plugin.js
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2012-2015 S-Core Co., Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,9 +24,11 @@
  *   plugin.js
  */
 
-define([ ], function () {
+define([
+    'dojo/i18n!./nls/resource',
+    'webida-lib/util/locale'
+], function (i18n, Locale) {
     'use strict';
-    
     var item = {
         'Help Contents' : {
             'IDE Reference Manual' : [ 'cmnd', 'plugins/help/help-commands', 'showHelpDocument' ],
@@ -34,6 +36,9 @@ define([ ], function () {
         },
         '&About': [ 'cmnd', 'plugins/help/help-commands', 'showAbout' ]
     };
+    var localizer = new Locale(i18n);
+    localizer.convertMenuItem(item, 'menu');
+    localizer.convertMenuItem(item['Help Contents'], '[menu] ');
 
     return {
         getViableItems : function () {
