d5af934263078a8283292ac8c9cc84c770ce3478
Koong Kyungmi <kyungmi.k@samsung.com>
2015-12-21 19:29:28 +0900

    [DOCUMENT] (#772, #535) Documentation for plugin-settings plugin
    [DESC.]
    - add JsDoc for public/private methods
    - clean up code and its dependencies
    - change the occurrences of `@returns` to `@return`

--- apps/ide/src/plugins/webida.plugin-setting/plugin-settings.js
+++ apps/ide/src/plugins/webida.plugin-setting/plugin-settings.js
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2012-2015 S-Core Co., Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,10 +15,9 @@
  */
 
 /**
- * plugin-manager's API wrapper for this plugin
- *
- * @since: 15. 10. 19
- * @author: Koong Kyungmi (kyungmi.k@samsung.com)
+ * @file plugin-manager's API wrapper for this plugin
+ * @since 1.5.0
+ * @author kyungmi.k@samsung.com
  */
 
 define([
@@ -30,15 +29,39 @@ define([
 ) {
     'use strict';
 
-    var plugins;    // for caching and update in real time.
+    /**
+     * @callback errorCallback
+     * @param {(Error|String)} [error]
+     */
 
+    /**
+     * @type {Array}
+     * Plugin list for caching and update in real time.
+     */
+    var plugins;
+
+    /**
+     * module main
+     * @type {Object}
+     */
     var mod = {
+        /**
+         * Get all plugins
+         * @return {Array}
+         * @see {plugin-manager-0.1#getAllPluginSettings}
+         */
         getPluginSettings: function () {
             if (!plugins) {
                 plugins = _.values(pluginManager.getAllPluginSettings());
             }
             return plugins;
         },
+        /**
+         * Save user specific plugin settings to the file(plugin-settings.json)
+         * @param {Array.<string>} disabledPlugins - list of the plugin locations to set disabled
+         * @param {errorCallback} callback
+         * @see {plugin-manager-0.1#setUserPluginSettings}
+         */
         saveUserPluginSettings: function (disabledPlugins, callback) {
             _.forEach(plugins, function (plugin) {
                 plugin.disabled = (disabledPlugins.indexOf(plugin.loc) > -1);
